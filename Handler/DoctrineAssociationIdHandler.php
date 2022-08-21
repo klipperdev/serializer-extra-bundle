0e8aaf6656322391c448e73a8bd5f25fc1051a16
François Pluchino <francois.pluchino@klipper.dev>
2022-08-21 18:14:33 +0200

    Add native return type declaration to methods of serializer subscribers

--- Handler/DoctrineAssociationIdHandler.php
+++ Handler/DoctrineAssociationIdHandler.php
@@ -29,7 +29,7 @@ class DoctrineAssociationIdHandler implements SubscribingHandlerInterface
         $this->doctrine = $doctrine;
     }
 
-    public static function getSubscribingMethods()
+    public static function getSubscribingMethods(): array
     {
         $methods = [];
 
